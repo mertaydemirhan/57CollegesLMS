19ffb77bd3ece3afe698664e54aa63c615327b34
mertay <mertay.demirhan@gmail.com>
2022-09-05 15:00:08 +0300

    First Commit

--- WebApplication1/SignUp.aspx.cs
+++ WebApplication1/SignUp.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace StudentA
 {
-    public partial class EklePage : System.Web.UI.Page
+    public partial class SignUp : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
